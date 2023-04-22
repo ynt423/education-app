b9a8ad61a21f9fdb5a4104ef06a8f3a39a93194a
ynt423 <oscar42302@gmail.com>
2023-04-22 14:19:50 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
   return (
     <Routes>
       <Route path="/education-app" element={<Login />} />
-      <Route path="/education-app/signup" element={<SignUp />} />
-      <Route path="/education-app/dashboard" element={<Dashboard />} />
+      <Route path="/education-app/SignUp" element={<SignUp />} />
+      <Route path="/education-app/Dashboard" element={<Dashboard />} />
       <Route path="/education-app/Card" element={<Card />} />
       {/* <Route path="*" element={<h1>404: Not Found</h1>} /> */}
     </Routes>
